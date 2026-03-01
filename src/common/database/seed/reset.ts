63ce5e47c0b5be52aec22306c7e1fc0d4cdbc10b
herifanantenana <herifanantenana17>
2026-03-01 08:02:14 +0300

    feat(database): add migration for accounts, actors, sessions, users, and organizations
    - Created new SQL migration file to define tables and enums for accounts, actors, sessions, users, and organizations.
    - Added foreign key constraints and indexes for better data integrity and performance.
    - Updated package.json scripts for database cleanup and reset operations.
    - Implemented clean and reset scripts to manage database state effectively.
    - Updated README.md to reflect changes in sponsorship badge visibility.
    - Enhanced VSCode settings for spell checking with additional words.

--- src/common/database/seed/reset.ts
+++ src/common/database/seed/reset.ts
@@ -7,22 +7,28 @@ dotenv.config({ path: envFile });
 const connString = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 const db = drizzle(connString);
 
-const tablesName = ["users", "accounts", "actors", "sessions", "organizations"];
-
 async function resetDatabase() {
-	console.log("<-- Resetting the database");
+	console.log("<--- Resetting the database");
 	try {
+		// deactivate the referential integrity to avoid issues with foreign keys
 		await db.execute(sql`SET session_replication_role = 'replica';`);
 
+		// fetch all table names in the public schema
+		const tablesResult = await db.execute(sql.raw(`SELECT tablename FROM pg_tables WHERE schemaname = 'public';`));
+		const tablesName = (tablesResult.rows as Array<{ tablename: string }>).map((row) => row.tablename);
+
+		// truncate all tables and restart identity to reset auto-incrementing primary keys
 		for (const table of tablesName) {
-			console.log(`Truncating table: ${table}`);
+			console.log(` -- Truncating table: ${table}`);
 			await db.execute(sql.raw(`TRUNCATE TABLE "${table}" RESTART IDENTITY CASCADE;`));
 		}
 
-		await db.execute(sql`SET session_replication_role = 'origin';`);
-		console.log("--> Database reset successfully.");
+		console.log("---> Database reset successfully.");
 	} catch (error) {
-		console.error("--> Error resetting the database:", error);
+		console.error("---> Error resetting the database:", error);
+	} finally {
+		// Reactivate the referential integrity in case of error
+		await db.execute(sql`SET session_replication_role = 'origin';`);
 	}
 }
 
