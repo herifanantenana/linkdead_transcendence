2955142766ca655381e90435ad2a4634594f9a2f
herifanantenana <herifanantenana17>
2026-03-01 12:42:21 +0300

    Remove console log for environment variable in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./common/database/database.module";
-console.log(`.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ""}`);
 
 @Module({
 	imports: [
